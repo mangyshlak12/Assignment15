30f93aa89ca824327c7c0c55441d7bddb0d652c1
Batyrkhan <syrymbatyrhan@gmail.com>
2023-12-23 21:34:09 +0300

    Batyr

---
+++ src/stories/Accordion.stories.js
@@ -0,0 +1,22 @@
+import Tooltip from '../components/Containers/Accordion.vue';
+
+//👇 This default export determines where your story goes in the story list
+export default {
+  component: Tooltip,
+};
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/api/csf
+ * to learn how to use render functions.
+ */
+export const FirstStory = {
+  render: (args, { argTypes }) => ({
+    components: { Tooltip },
+    props: Object.keys(argTypes),
+    template: '<Tooltip v-bind="$props" />',
+  }),
+  args: {
+    //👇 The args you need here will depend on your component
+  },
+};
